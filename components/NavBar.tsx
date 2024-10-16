b387bc099bd35a7e4012564c1f73642f096e66ed
PixelSorcerer90 <158556591+Pongal1059@users.noreply.github.com>
2024-10-16 19:15:12 +0530

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ConnectButton from "@/components/ConnectButton"; // Adjust the import based on your file structure
+import Image from "next/image"; // Import Image from next/image
 
 const NavBar = () => {
   return (
     <div className="flex justify-between items-center p-4 bg-opacity-90 backdrop-blur-md bg-gray-800 text-white shadow-lg rounded-lg">
       <div className="flex items-center">
-        <img
+        <Image
           src="/logo.png"
           alt="Logo"
           width={50}
